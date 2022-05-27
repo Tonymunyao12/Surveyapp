dee566c0b515dd63417231cff5a19bedbd1fb8b3
Mulatya <munyaotony12@gmail.com>
2022-05-27 18:27:07 +0300

    Implementation of the user database.

--- app/src/main/java/com/mulatya/surveyapp/datalayer/UserDatabase.kt
+++ app/src/main/java/com/mulatya/surveyapp/datalayer/UserDatabase.kt
@@ -1,8 +1,40 @@
 package com.mulatya.surveyapp.datalayer
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
 
 @Database(entities = [UserEntity::class], version = 1)
- abstract class UserDatabase {
+ abstract class UserDatabase : RoomDatabase() {
 
+   abstract val userDatabaseDao: UserDatabaseDao
+
+   companion object {
+
+    @Volatile
+     private var INSTANCE: UserDatabase? = null
+
+     fun getInstance(context: Context): UserDatabase?{
+         synchronized(this) {
+
+             var instance = INSTANCE
+
+             if (instance == null) {
+                 instance = Room.databaseBuilder(
+                     context.applicationContext,
+                     UserDatabase::class.java,
+                     "user_details_database"
+                 )
+                     .fallbackToDestructiveMigration()
+                     .build()
+
+                 INSTANCE = instance
+             }
+             return instance
+         }
+     }
+   }
 }
