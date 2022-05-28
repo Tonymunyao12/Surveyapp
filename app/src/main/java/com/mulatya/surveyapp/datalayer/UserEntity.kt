2304ac7f7aac44189fda4dec6582152e3789994a
Mulatya <munyaotony12@gmail.com>
2022-05-28 19:36:56 +0300

    Update of the entity class.

--- app/src/main/java/com/mulatya/surveyapp/datalayer/UserEntity.kt
+++ app/src/main/java/com/mulatya/surveyapp/datalayer/UserEntity.kt
@@ -1,5 +1,6 @@
 package com.mulatya.surveyapp.datalayer
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -8,4 +9,17 @@ data class UserEntity(
 
    @PrimaryKey(autoGenerate = true)
    var userId: Int = 0,
+
+   @ColumnInfo(name = "full_name")
+   var fullName: String,
+
+   @ColumnInfo(name = "phone_number")
+   var phoneNumber: Int,
+
+   @ColumnInfo(name = "gender")
+   var gender: String,
+
+   @ColumnInfo(name = "size_of_farm")
+   var farmSize: Float,
+
 )
