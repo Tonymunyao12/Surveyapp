2aff743efc881d2c34d165b6bec71803c28a1939
Mulatya <munyaotony12@gmail.com>
2022-06-08 15:50:54 +0300

    Updated Main Activity file.

--- app/src/main/java/com/mulatya/surveyapp/MainActivity.kt
+++ app/src/main/java/com/mulatya/surveyapp/MainActivity.kt
@@ -1,10 +1,32 @@
+/*
+* Copyright 2020 BlessedCoders
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+* */
+
 package com.mulatya.surveyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import com.mulatya.lib.PhoneNumberKit
 import com.mulatya.surveyapp.databinding.ActivityMainBinding
+
+/*
+* Acts as the Login Activity.
+* */
 
 class MainActivity : AppCompatActivity() {
 
@@ -45,6 +67,13 @@ class MainActivity : AppCompatActivity() {
             defaultRegion = "tr"
         )
         Log.d(SignupActivity.TAG, "Formatted Number: $formattedNumber")
+
+        // New users to navigate to Signup Activity.
+        val navigateToSignup: TextView = findViewById(R.id.register_txt)
+        navigateToSignup.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
 
 
     }
